e59494274ce34776462bc6e151715fcc78657899
madan-webonise <madan.somvanshi@weboniselab.com>
2018-04-18 12:27:37 +0530

    New Route File

---
+++ my-app/src/Route1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './assets/styles/layout.css';
+
+class Route1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Peter'
+    }
+  }
+}
+
+export default Route1;
